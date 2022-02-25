118fb90075a1465dd24dce29699560a5826a3239
Bruno Bouwman <brunobouwman@hotmail.com>
2022-02-25 22:59:23 +0000

    location by address fucntionality working, missing share a place link and overall app styling

--- src/SharePlace.js
+++ src/SharePlace.js
@@ -1,11 +1,12 @@
 import { Modal } from './UI/Modal';
 import { Map } from './UI/Map';
+import {getCoordsFromAddress} from './Utility/Location'
 
 class PlaceFinder {
     constructor() {
-      const addressForm = document.querySelector('form');
+      const addressFormBtn = document.querySelector('form button');
       const locateUserBtn = document.getElementById('locate-btn');
-      addressForm.addEventListener('click', this.locateUserForm.bind(this));
+      addressFormBtn.addEventListener('click', this.findAdressHandler.bind(this));
       locateUserBtn.addEventListener('click', this.locateUserHandler.bind(this)); //adding eventlisteners to the constructor so they will be initialized in the instance (bind this so the handler function can utilize 'this' related to the class instance)
     }
 
@@ -42,7 +43,24 @@ class PlaceFinder {
       );
     }
   
-    findAdressHandler() {}
+    async findAdressHandler(event) {
+      // console.log(event.target.parentElement.querySelector('input'));
+      event.preventDefault(); //prevents the default submit event by the browser (reload the page)
+      const address = event.target.parentElement.querySelector('input').value; //target is the form itself
+      if (!address || address.trim().lenght === 0) { //checks if the address was inputed (trim to ignore white spaces)
+       alert('Invalid address entered, try again');
+       return; 
+      }
+      const modal = new Modal('loading-modal-content', 'Loading location - please wait');
+      modal.show();
+      try {
+     const coordinates = await getCoordsFromAddress(address); //returns a promise
+     this.selectPlace(coordinates);
+      }catch (err) { //catches any potential error in the proccess
+        alert(err.message); //built-in error constructor function gives you the .message property (display messages created in the error handling configured in the location.js file)
+      }
+      modal.hide(); //only executes when the promise settled (wrapped into an 'invisble' then block)
+      }
   }
   
   new PlaceFinder();
